b3925f8dcf4006a5d9c76b09bc0b0ac2edadfed8
yangdong <yangdong@guotailimin.com>
2020-11-16 19:38:25 +0800

    完善sdk

--- src/main.js
+++ src/main.js
@@ -1,93 +1,70 @@
-import {captureError,proxyFetch,proxyXhr} from './capture'
-import {reportData} from './report'
+import { captureError, proxyFetch, proxyXhr } from "./capture";
+import { reportData } from "./report";
 
-function main(option,fn){
-  let filterUrl = ["livereload.js?snipver=1", "/sockjs-node/"];
-  let opt = {
-    // 上报地址
-    domain: "http://localhost/api",
-    // 脚本延迟上报时间
-    delay: 300,
-    // 是否是单页面应用
-    isSPA: true,
-    // 是否上报页面性能数据
-    isReportPerformance: true,
-    // 是否上报错误信息
-    isReportError: true,
-    // 请求时需要过滤的url信息
-    filterUrl: [],
-    // 是否上报请求接口性能数据
-    isReportApiRequest: true,
-    // 其他上报
-    add: {}
-  };
-  opt = Object.assign(opt, option);
-  opt.filterUrl = opt.filterUrl.concat(filterUrl);
+function main(option) {
+	let filterUrl = ["livereload.js?snipver=1", "/sockjs-node/"];
+	let opt = {
+		// 上报地址
+		domain: "http://localhost/api",
+		// 脚本延迟上报时间
+		delay: 300,
+		// 是否是单页面应用
+		isSPA: true,
+		// 是否上报页面性能数据
+		isReportPerformance: true,
+		// 是否上报错误信息
+		isReportError: true,
+		// 请求时需要过滤的url信息
+		filterUrl: [],
+		// 是否上报请求接口性能数据
+		isReportApiRequest: true,
+		// 支持特殊自定义上报方式
+		fn: null,
+		// 其他上报
+		add: {}
+	};
+	opt = Object.assign(opt, option);
+	opt.filterUrl = opt.filterUrl.concat(filterUrl);
 
-  const conf = {
-    // 页面性能列表
-    performance: {},
-    // 错误列表
-    errorList: [],
-    // 接口请求列表
-    requestList: [],
+	// error上报
+	if (opt.isReportError) captureError();
 
-    // 页面fetch数量
-    fetchNum: 0,
-    // ajax onload数量
-    loadNum: 0,
-    // 页面ajax数量
-    ajaxLength: 0,
-    // 页面fetch总数量
-    fetLength: 0,
-    // 页面ajax信息
-    ajaxMsg: {},
-    // ajax成功执行函数
-    goingType: "",
-    // 是否有ajax
-    haveAjax: false,
-    // 是否有fetch
-    haveFetch: false,
-    // 来自域名
-    preUrl: document.referrer && document.referrer !== location.href ? document.referrer : "",
-    // 当前页面
-    page: ""
-  };
-  // error default
-  const errorInfo = {
-    type: "",
-    data: {
-      msg: "",
-      url: "",
-      stack: ""
-    }
-  };
-  const requestInfo = {
-    url: "",
-    statusCode: 0,
-    timeConsuming: 0,
-    responseMsg: ""
-  };
+	// 非SPA上报页面性能数据
+	if (!opt.isSPA) {
+		addEventListener(
+			"load",
+			function () {
+				reportData(opt, "performance");
+			},
+			false
+		);
+	}
 
-  // error上报
-  if (opt.isReportError) captureError();
-
-  // 非SPA上报页面性能数据
-  if (!opt.isSPA) {
-    addEventListener(
-      "load",
-      function () {
-        reportData(opt,"performance");
-      },
-      false
-    );
-  }
-
-  if (opt.isReportApiRequest) {
-    // 拦截fetch请求
-    proxyFetch(opt);
-    // 拦截xhr请求
-    proxyXhr(opt);
-  }
+	if (opt.isReportApiRequest) {
+		// 拦截fetch请求
+		proxyFetch(opt);
+		// 拦截xhr请求
+		proxyXhr(opt);
+	}
 }
-export default main
+// 提供给第三方上报错误信息
+main.addError = function (err) {
+	err = {
+		type: err.type,
+		data: {
+			url: err.data.url,
+			msg: err.data.msg,
+			stack: err.data.stack,
+			col: err.col || 0,
+			line: err.line || 0
+		}
+	};
+	main.ERRORLIST.push(err);
+};
+main.addData = function (fn) {
+	fn && fn(ADDDATA);
+};
+main.ERRORLIST = [];
+main.ADDDATA = {};
+
+export default main;
--- src/report.js
+++ src/report.js
@@ -1,62 +1,79 @@
-import {perforPage} from './capture'
-// performance:页面级性能上报
-// request:接口性能上报
-// error：页面错误信息上报
-export function reportData(opt,type) {
-  setTimeout(() => {
-    if (Performance.ERRORLIST && Performance.ERRORLIST.length) {
-      conf.errorList = conf.errorList.concat(Performance.ERRORLIST);
-    }
-    let result = {
-      addData: ADDDATA,
-      platform: getPlatform(),
-      UA: navigator.userAgent,
-      url: location.href
-    };
-
-    if (type === "performance") {
-      if (opt.isReportPerformance) perforPage();
-      result = Object.assign(result, {
-        // preUrl: conf.preUrl,
-        performance: conf.performance,
-        requestList: conf.requestList
-      });
-    } else if (type === "request") {
-      if (conf.requestList.length === 0) return false;
-      result = Object.assign(result, {
-        requestList: conf.requestList
-      });
-    }
-    result = Object.assign(result, {
-      errorList: conf.errorList
-    });
-
-    result = Object.assign(result, opt.add);
-    fn && fn(result);
-    if (!fn && navigator.sendBeacon) {
-      navigator.sendBeacon(opt.domain, JSON.stringify(result));
-    } else {
-      const xhr = new XMLHttpRequest();
-      xhr.open("POST", opt.domain, true);
-      xhr.send(JSON.stringify(result));
-    }
-    // 清空无关数据
-    Promise.resolve().then(() => {
-      clear();
-    });
-  }, opt.delay);
-}
+import main from "./main";
+import { perforPage, conf } from "./capture";
 
 // 获取平台信息 PC(chrome,IE,firefox) H5(ios,android,ios_webview,android_webview,weChart)
 // 暂时只区分PC和H5
 function getPlatform() {
-  if (
-    window.navigator.userAgent.match(
-      /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
-    )
-  ) {
-    return "H5"; // 移动端
-  } else {
-    return "PC"; // PC端
-  }
+	if (
+		window.navigator.userAgent.match(
+			/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
+		)
+	) {
+		return "H5"; // 移动端
+	} else {
+		return "PC"; // PC端
+	}
+}
+
+function clear(type = 0) {
+	if (window.performance && window.performance.clearResourceTimings) performance.clearResourceTimings();
+	conf.performance = {};
+	conf.errorList = [];
+	conf.requestList = [];
+	conf.preUrl = "";
+	conf.resourceList = [];
+	conf.page = type === 0 ? location.href : "";
+	conf.haveAjax = false;
+	conf.haveFetch = false;
+	conf.ajaxMsg = {};
+	main.ERRORLIST = [];
+	main.ADDDATA = {};
+}
+
+// performance:页面级性能上报
+// request:接口性能上报
+// error：页面错误信息上报
+export function reportData(opt, type) {
+	setTimeout(() => {
+		if (Performance.ERRORLIST && Performance.ERRORLIST.length) {
+			conf.errorList = conf.errorList.concat(Performance.ERRORLIST);
+		}
+		let result = {
+			addData: main.ADDDATA,
+			platform: getPlatform(),
+			UA: navigator.userAgent,
+			url: location.href
+		};
+
+		if (type === "performance") {
+			if (opt.isReportPerformance) perforPage();
+			result = Object.assign(result, {
+				// preUrl: conf.preUrl,
+				performance: conf.performance,
+				requestList: conf.requestList
+			});
+		} else if (type === "request") {
+			if (conf.requestList.length === 0) return false;
+			result = Object.assign(result, {
+				requestList: conf.requestList
+			});
+		}
+		result = Object.assign(result, {
+			errorList: conf.errorList
+		});
+
+		result = Object.assign(result, opt.add);
+		opt.fn && opt.fn(result);
+		if (!opt.fn && navigator.sendBeacon) {
+			navigator.sendBeacon(opt.domain, JSON.stringify(result));
+		} else {
+			const xhr = new XMLHttpRequest();
+			xhr.open("POST", opt.domain, true);
+			xhr.send(JSON.stringify(result));
+		}
+		// 清空无关数据
+		Promise.resolve().then(() => {
+			clear();
+		});
+	}, opt.delay);
 }
